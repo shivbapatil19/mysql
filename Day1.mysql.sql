b9d8c7d82c69fdf990ede9ea618795a5de4d2c4f
shivbapatil19 <patilshivba03@gmail.com>
2025-08-25 03:25:51 -0700

    Add files via upload

---
+++ Day1.mysql.sql
@@ -0,0 +1,14 @@
+create database if not exists employees;
+##here a simple database name as employees has been created
+
+create database if not exists students;
+##here again the database as a name of student is being created 
+
+## in databases their are 4 functions :- table views stored procedures functions;
+
+
+use employees;
+##it will use database name as employees
+
+use students;
+##it will use database name as students 
